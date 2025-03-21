529eebceafd400fb50018958a7314b42850c348c
Riyeth Angco <riyeth@gmail.com>
2025-03-20 20:22:31 -0400

    code cleanup

--- 02-05-authn-jwt/src/booking/booking.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { BookingController } from './booking.controller';
-import { AuthModule } from '../auth/auth.module';
-
-@Module({
-  imports: [AuthModule],
-  controllers: [BookingController],
-})
-export class BookingModule {}
