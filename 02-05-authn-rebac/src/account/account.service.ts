9206af5ed5edc72e5dadaa528620d98a6fdcb596
Riyeth Angco <riyeth@gmail.com>
2025-03-20 22:44:32 -0400

    working ReBAC code for section 2.2

---
+++ 02-05-authn-rebac/src/account/account.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AccountService {
+    private accounts = [
+        { id: 1, customer: 'jane_smith', account_id: 'AA123', account_type: "chequing", balance: 1000.00},
+        { id: 2, customer: 'jane_smith', account_id: 'BA456', account_type: "savings", balance: 5000.00},
+        { id: 3, customer: 'bob_smith', account_id: 'CD765', account_type: "savings", balance: 3000.00},
+        { id: 4, customer: 'john_davis', account_id: 'YT432', account_type: "savings", balance: 2000.00},
+      ];
+
+  getAccount(username: string) {
+    const account = this.accounts.filter(a => a.customer == username);
+    return account; 
+  }
+
+  createAccount() {
+    return "Account successfully created.";
+  }
+
+}
---
+++ 02-05-authn-rebac/src/auth/jwt.strategy.ts
@@ -0,0 +1,19 @@
+import { Strategy } from 'passport-jwt';
+import { ExtractJwt } from 'passport-jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable } from '@nestjs/common';
+import { jwtConstants } from './constants';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: jwtConstants.secret,  // This should be stored in an environment variable
+    });
+  }
+
+  async validate(payload: any) {
+    return { userId: payload.sub, username: payload.username, role: payload.role };
+  }
+}
