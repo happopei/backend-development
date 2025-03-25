e4ad94c72c77a89e20bbff8a641d9b3841019fab
Riyeth Angco <riyeth@gmail.com>
2025-03-25 10:59:12 -0400

    initial jwt flask

---
+++ 02-05-authn-flask-jwt/app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_jwt_extended import JWTManager
+from routes.auth import auth_blueprint
+from routes.account import account_blueprint
+
+app = Flask(__name__)
+app.config["JWT_SECRET_KEY"] = "super-secret"  # Replace in prod!
+jwt = JWTManager(app)
+
+# Register blueprints (modules)
+app.register_blueprint(auth_blueprint, url_prefix='/auth')
+app.register_blueprint(account_blueprint, url_prefix='/accounts')
+
+if __name__ == '__main__':
+    app.run(debug=True, host='localhost')
---
+++ 02-05-authn-flask-jwt/routes/account.py
@@ -0,0 +1,18 @@
+from flask import Blueprint, jsonify
+from flask_jwt_extended import jwt_required, get_jwt_identity
+# from utils.guards import role_required
+
+account_blueprint = Blueprint('account', __name__)
+
+accounts = [
+    {"customer": "jane_smith", "account_id": "AA123", "balance": 1000},
+    {"customer": "bob_smith", "account_id": "BB456", "balance": 2000}
+]
+
+@account_blueprint.route('', methods=['GET'])
+@jwt_required()
+# @role_required('customer')  # Only customers can view their accounts
+def view_account():
+    current_user = get_jwt_identity()
+    user_accounts = [a for a in accounts if a['customer'] == current_user['username']]
+    return jsonify(user_accounts)
