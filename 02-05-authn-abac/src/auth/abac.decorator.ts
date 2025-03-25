6b5f610eac2723dfcc2faade6d577d89a5fef835
Riyeth Angco <riyeth@gmail.com>
2025-03-25 10:58:51 -0400

    cleanup

--- 02-05-authn-abac/src/auth/abac.decorator.ts
+++ 02-05-authn-abac/src/auth/abac.decorator.ts
@@ -1,5 +1,5 @@
 // Next, let's create a metadata decorator to indicate ABAC requirements
-// src/decorators/require-abac.decorator.ts
+
 import { SetMetadata } from '@nestjs/common';
 
 export const ABAC_KEY = 'abac';
