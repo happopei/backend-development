9e7bfa6aaec80becedefb3e32fba77b621be48bf
Riyeth Angco <riyeth@gmail.com>
2025-03-27 11:27:06 -0400

    flask abac

---
+++ 02-05-authn-flask-abac/routes/auth.py
@@ -0,0 +1,22 @@
+from flask import Blueprint, request, jsonify
+from flask_jwt_extended import create_access_token
+
+auth_blueprint = Blueprint('auth', __name__)
+
+users = [
+    {"username": "alice_teller", "password": "teller123", "role": "teller"},
+    {"username": "jane_smith", "password": "password", "role": "customer"}
+]
+
+@auth_blueprint.route('/login', methods=['POST'])
+def login():
+    username = request.json.get('username')
+    password = request.json.get('password')
+
+    print(f"Current User: {username}") 
+    user = next((u for u in users if u['username'] == username), None)
+    if username == user["username"] and password == user["password"]:
+        access_token = create_access_token(identity=user['username'], additional_claims={"role": user['role']})
+        return jsonify(access_token=access_token), 200
+    else:
+        return jsonify({"msg": "Invalid credentials"}), 401
