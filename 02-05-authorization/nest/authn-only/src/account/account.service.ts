41eaf5e7ae6ce772d066f84170cc56797ce4dc5a
Jerome Ortega <ortegajerome@gmail.com>
2025-03-30 18:02:55 +0000

    add jane smit account

--- 02-05-authorization/nest/authn-only/src/account/account.service.ts
+++ 02-05-authorization/nest/authn-only/src/account/account.service.ts
@@ -2,13 +2,14 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AccountService {
-    private accounts = [
-        { id: 1, customer: 'jane_doe', account_id: 'AA123', account_type: "chequing", balance: 1000.00},
-        { id: 2, customer: 'jane_doe', account_id: 'BA456', account_type: "savings", balance: 5000.00},
-      ];
+  private accounts = [
+    { id: 1, customer: 'jane_doe', account_id: 'AA123', account_type: "chequing", balance: 1000.00 },
+    { id: 2, customer: 'jane_doe', account_id: 'BA456', account_type: "savings", balance: 5000.00 },
+    { id: 2, customer: 'jane_smith', account_id: 'BA344', account_type: "savings", balance: 5000.00 },
+  ];
 
   getAccount() {
-    return this.accounts; 
+    return this.accounts;
   }
 
   createAccount() {
