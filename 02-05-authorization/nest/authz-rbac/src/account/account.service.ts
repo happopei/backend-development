1e3b3c0f4d750a05a702120a2b1703defee5c1a5
Jerome Ortega <ortegajerome@gmail.com>
2025-03-30 18:49:00 +0000

    add jane doe

--- 02-05-authorization/nest/authz-rbac/src/account/account.service.ts
+++ 02-05-authorization/nest/authz-rbac/src/account/account.service.ts
@@ -2,15 +2,15 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AccountService {
-    private accounts = [
-        { id: 1, customer: 'jane_smith', account_id: 'AA123', account_type: "chequing", balance: 1000.00},
-        { id: 2, customer: 'jane_smith', account_id: 'BA456', account_type: "savings", balance: 5000.00},
-        { id: 3, customer: 'bob_smith', account_id: 'CD765', account_type: "savings", balance: 3000.00},
-        { id: 4, customer: 'john_davis', account_id: 'YT432', account_type: "savings", balance: 2000.00},
-      ];
+  private accounts = [
+    { id: 1, customer: 'jane_smith', account_id: 'AA123', account_type: "chequing", balance: 1000.00 },
+    { id: 2, customer: 'jane_smith', account_id: 'BA456', account_type: "savings", balance: 5000.00 },
+    { id: 3, customer: 'jane_doe', account_id: 'CD765', account_type: "savings", balance: 3000.00 },
+    { id: 4, customer: 'john_davis', account_id: 'YT432', account_type: "savings", balance: 2000.00 },
+  ];
 
   getAccount() {
-    return this.accounts; 
+    return this.accounts;
   }
 
   createAccount() {
